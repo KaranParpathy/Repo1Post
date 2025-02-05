9f484a38a80368c67dc38774c1415443d7ef47c6
KaranParpathy <karanparpathy@gmal.com>
2025-02-05 21:19:44 +0530

    demo.js file added

--- JavaScript/demo.js
+++ JavaScript/demo.js
@@ -1,4 +1,10 @@
 console.log("Hello World");
-let int,age=10;
+let int,age=1;
 let String, name="Karan";
 console.log(age);console.log(name);
+if(age>=10){
+    console.log("Age is Greater than or equal to 10");
+}
+else{
+    console.log("Age is 1");
+}
